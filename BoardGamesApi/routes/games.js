5290c3d2a88915c52257380b929d3c1c1d21b906
Kacper Kruger <kacper.kruger1@gmail.com>
2025-02-14 14:52:09 +0100

    removed console log

--- BoardGamesApi/routes/games.js
+++ BoardGamesApi/routes/games.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
 
-console.log("test");
 
 const Game = require('../models/Game');
 const Publisher = require('../models/Publisher');
