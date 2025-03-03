8cb58972f39b661ae96d457cc5ebcefe39f27397
Kacper Kruger <kacper.kruger1@gmail.com>
2025-03-03 16:21:34 +0100

    changed home page message

--- BoardGamesApp/src/index.js
+++ BoardGamesApp/src/index.js
@@ -13,8 +13,6 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
 
 
-console.log("Hello World");
-
 const store = createStore(
   combineReducers({
     games: gamesReducer,
